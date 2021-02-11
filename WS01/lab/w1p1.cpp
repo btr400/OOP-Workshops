e208fcc3994001b921be36d26e6634fcb8a73c59
BTR400 <gean400@gmail.com>
2021-02-11 12:55:26 -0500

    aa

--- WS01/lab/w1p1.cpp
+++ WS01/lab/w1p1.cpp
@@ -6,7 +6,7 @@
 // w1p1.cpp
 ///////////////////////////////////////////////////
 #define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
+#include "Header Files/File.h"
 #include <iostream>
 #include <cstdlib>
 
@@ -24,20 +24,20 @@ void readCstr(char cstr[], int len);
 int readInt(int min, int max);
 
 
-const char* const SHOPPING_DATA_FILE = "shoppinglist.csv";
+// const char* const SHOPPING_DATA_FILE = "shoppinglist.csv";
 FILE* sfptr = nullptr;
 
 
 
-const int MAX_TITLE_LENGTH = 50;
-const int MAX_QUANTITY_VALUE = 50;
-struct ShoppingRec {
-   char m_title[MAX_TITLE_LENGTH + 1];
-   int m_quantity;
-   bool m_bought;
-};
+//const int MAX_TITLE_LENGTH = 50;
+//const int MAX_QUANTITY_VALUE = 50;
+//struct ShoppingRec {
+//   char m_title[MAX_TITLE_LENGTH + 1];
+//   int m_quantity;
+//   bool m_bought;
+//};
 
-const int MAX_NO_OF_RECS = 15;
+//const int MAX_NO_OF_RECS = 15;
 ShoppingRec recs[MAX_NO_OF_RECS] = {};
 int noOfRecs = 0;
 
@@ -47,11 +47,6 @@ void displayShoppingRec(const ShoppingRec* shp);
 void toggleBoughtFlag(ShoppingRec* rec);
 bool isShoppingRecEmpty(const ShoppingRec* shp);
 
-bool openFileForRead();
-bool openFileForOverwrite();
-void closeFile();
-bool freadShoppingRec(ShoppingRec* rec);
-void fwriteShoppintRec(const ShoppingRec* rec);
 
 bool loadList();
 void displayList();
@@ -293,28 +288,28 @@ void removeBoughtItems() {
 
 
 
-bool openFileForRead() {
-   sfptr = fopen(SHOPPING_DATA_FILE, "r");
-   return sfptr != NULL;
-}
-
-bool openFileForOverwrite() {
-   sfptr = fopen(SHOPPING_DATA_FILE, "w");
-   return sfptr != NULL;
-}
-
-void closeFile() {
-   if (sfptr) fclose(sfptr);
-}
-
-bool freadShoppingRec(ShoppingRec* rec) {
-   int flag = 0;
-   bool success = fscanf(sfptr, "%[^,],%d,%d\n",
-      rec->m_title, &rec->m_quantity, &flag) == 3;
-   rec->m_bought = !!flag;
-   return success;
-}
-
-void fwriteShoppintRec(const ShoppingRec* rec) {
-   fprintf(sfptr, "%s,%d,%d\n", rec->m_title, rec->m_quantity, rec->m_bought);
-}
+//bool openFileForRead() {
+//   sfptr = fopen(SHOPPING_DATA_FILE, "r");
+//   return sfptr != NULL;
+//}
+//
+//bool openFileForOverwrite() {
+//   sfptr = fopen(SHOPPING_DATA_FILE, "w");
+//   return sfptr != NULL;
+//}
+//
+//void closeFile() {
+//   if (sfptr) fclose(sfptr);
+//}
+//
+//bool freadShoppingRec(ShoppingRec* rec) {
+//   int flag = 0;
+//   bool success = fscanf(sfptr, "%[^,],%d,%d\n",
+//      rec->m_title, &rec->m_quantity, &flag) == 3;
+//   rec->m_bought = !!flag;
+//   return success;
+//}
+//
+//void fwriteShoppintRec(const ShoppingRec* rec) {
+//   fprintf(sfptr, "%s,%d,%d\n", rec->m_title, rec->m_quantity, rec->m_bought);
+//}
